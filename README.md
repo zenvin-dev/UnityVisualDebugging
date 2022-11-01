00c536db475be50a7b00a845a69cc7c9268203d7
Zenvin <zenvin@outlook.de>
2022-11-01 01:21:17 +0100

    - Add `ValueDebugger`

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Unity Visual Debugging
-Small package for drawing debug lines & shapes in play mode. \
-Uses pooled [line renderers](https://docs.unity3d.com/ScriptReference/LineRenderer.html) for rendering.
+Small package for drawing debug lines & shapes, as well as debugging variable values during runtime.
 
-## Usage
+---
+## Visual Debugger
+Uses pooled [line renderers](https://docs.unity3d.com/ScriptReference/LineRenderer.html) for rendering wireframe shapes in a scene.
+
+### Usage
 Just like with [`UnityEngine.Debug`](https://docs.unity3d.com/ScriptReference/Debug.html) methods, simply call the desired method(s) of `Zenvin.VisualDebugging.VisualDebugger` once a frame for every shape you want to be drawn.
 
-## Draw methods list
+### Draw methods list
 * `DrawCircle` - Draws a circle at a given position, a given rotation and a given number of vertices.
 * `DrawLine` - Draws a line between two points.
 * `DrawPath` - Draws a path between any number of points.
 * `DrawRay` - Draws a line starting at a given point, going in a given direction for a given length.
 * `DrawRectangle` - Draws a rectangle of a given size at a given position.
 * `DrawSphere` - Draws a wire sphere of a given size at a given position. The circles making up the sphere have 32 vertices each.
+
+## Value Debugger
+Allows the display of labelled values via [`OnGUI`](https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnGUI.html). \
+[`Func<string>`](https://learn.microsoft.com/en-us/dotnet/api/system.func-1?view=net-7.0) delegate methods are used to update displayed values in a specific interval. This defaults to once every 0.25 seconds, but may be set manually using `SetUpdateInterval(float)`
+
+### Usage
+To display a value, simply call the `static` method `RegisterTarget(DebugTarget)` on `Zenvin.VisualDebugging.ValueDebugger`, passing it a valid target.
+```csharp
+Zenvin.VisualDebugging.ValueDebugger.RegisterTarget(new DebugTarget(Foo, "Some Value"));    // assuming Foo is a method with return type string.
+```
+The `RegisterTarget(DebugTarget)` method will return a numeric ID, which can be used to remove the value from display again using `RemoveTarget(int)`.
---
+++ Runtime/ValueDebugger.cs
@@ -0,0 +1,231 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace Zenvin.VisualDebugging {
+	public class ValueDebugger : MonoBehaviour {
+
+		public static readonly CultureInfo Culture = CultureInfo.GetCultureInfo ("en-US") ?? CultureInfo.CurrentCulture;
+
+		private static ValueDebugger debugger;
+		private static int referenceID = 0;
+
+		private readonly Dictionary<int, DebugTarget> debugTargets = new Dictionary<int, DebugTarget> ();
+		private readonly List<(string Label, string Value)> targetValues = new List<(string Label, string Value)> ();
+
+		private Coroutine coroutine;
+		private GUIStyle labelStyle;
+		private GUIStyle valueStyle;
+
+
+		private float updateInterval = 0f;
+		private float spacing = 5f;
+		private float margin = 5f;
+
+		private Vector2 cellSize = new Vector2 (150, 40);
+
+
+		public static Vector2 CellSize {
+			get {
+				if (debugger == null) {
+					return Vector2.zero;
+				}
+				return debugger.cellSize;
+			}
+			set {
+				if (debugger != null) {
+					value.x = Mathf.Max (value.x, 10);
+					value.y = Mathf.Max (value.y, 40);
+					debugger.cellSize = value;
+				}
+			}
+		}
+
+		public static float Spacing {
+			get {
+				if (debugger == null) {
+					return 0f;
+				}
+				return debugger.spacing;
+			}
+			set {
+				if (debugger != null) {
+					debugger.spacing = Mathf.Max (0f, value);
+				}
+			}
+		}
+
+		public static float Margin {
+			get {
+				if (debugger == null) {
+					return 0f;
+				}
+				return debugger.margin;
+			}
+			set {
+				if (debugger != null) {
+					debugger.margin = Mathf.Max (0f, value);
+				}
+			}
+		}
+
+
+		public static bool SetUpdateInterval (float interval) {
+			if (interval <= 0f) {
+				return false;
+			}
+			Initialize ();
+			debugger.SetUpdateIntervalInternal (interval);
+			return true;
+		}
+
+		public static int RegisterTarget (DebugTarget target) {
+			if (!target.Valid) {
+				return -1;
+			}
+			Initialize ();
+			debugger.debugTargets[referenceID] = target;
+			return referenceID++;
+		}
+
+		public static void UnregisterTarget (int id) {
+			debugger?.debugTargets?.Remove (id);
+		}
+
+
+
+		private void OnGUI () {
+			if (!Debug.isDebugBuild) {
+				return;
+			}
+
+			if (labelStyle == null || valueStyle == null) {
+				SetupStyles ();
+			}
+
+			Rect screen = new Rect (margin, margin, Screen.width - margin * 2f, Screen.height - margin * 2f);
+			float cellWidth = (screen.width - spacing * cellSize.x) / cellSize.x;
+			float cellHeight = (screen.height - spacing * cellSize.y) / cellSize.y;
+
+			Vector2Int pos = Vector2Int.zero;
+			for (int i = 0; i < targetValues.Count; i++) {
+				Rect cell = GetCellRect (pos);
+				if (cell.y + cell.height > screen.y + screen.height) {
+					pos.x++;
+					pos.y = 0;
+					cell = GetCellRect (pos);
+				}
+
+				DrawCell (cell, i);
+				pos.y++;
+			}
+		}
+
+		private void DrawCell (Rect rect, int index) {
+			const float frame = 3f;
+			const float header = 15f;
+			var target = targetValues[index];
+
+			GUI.Box (rect, string.Empty);
+
+			rect.x += frame;
+			rect.width -= frame * 2f;
+			rect.y += frame;
+			rect.height -= frame * 2f;
+
+			Rect labelRect = new Rect (rect);
+			labelRect.height = header;
+
+			Rect valueRect = new Rect (rect);
+			valueRect.y += header;
+			valueRect.height -= header;
+
+			GUI.Label (labelRect, target.Label, labelStyle);
+			GUI.Label (valueRect, target.Value, valueStyle);
+		}
+
+		private Rect GetCellRect (Vector2Int pos) {
+			return new Rect (
+				margin + ((cellSize.x + spacing) * pos.x),
+				margin + ((cellSize.y + spacing) * pos.y),
+				cellSize.x,
+				cellSize.y
+			);
+		}
+
+
+		private static void Initialize () {
+			if (debugger != null) {
+				return;
+			}
+			GameObject obj = new GameObject ("[GUI Debugger]");
+			obj.hideFlags = HideFlags.HideInHierarchy;
+			DontDestroyOnLoad (obj);
+
+			debugger = obj.AddComponent<ValueDebugger> ();
+			//debugger.SetupStyles ();
+			debugger.SetUpdateIntervalInternal (0.25f);
+		}
+
+		private void SetupStyles () {
+			valueStyle = new GUIStyle (GUI.skin.label) {
+				fontSize = 14,
+				alignment = TextAnchor.MiddleRight,
+				margin = new RectOffset (),
+				padding = new RectOffset ()
+			};
+			labelStyle = new GUIStyle (GUI.skin.label) {
+				fontSize = 11,
+				fontStyle = FontStyle.Bold,
+				alignment = TextAnchor.MiddleLeft,
+				margin = new RectOffset (),
+				padding = new RectOffset ()
+			};
+		}
+
+		private void SetUpdateIntervalInternal (float interval) {
+			if (Mathf.Approximately (interval, updateInterval)) {
+				return;
+			}
+			if (coroutine != null) {
+				StopCoroutine (coroutine);
+			}
+			updateInterval = interval;
+			coroutine = StartCoroutine (UpdateValues (interval));
+		}
+
+		private IEnumerator UpdateValues (float interval) {
+			WaitForSecondsRealtime wfs = new WaitForSecondsRealtime (interval);
+			while (true) {
+				targetValues.Clear ();
+				List<DebugTarget> targets = new List<DebugTarget> (debugTargets.Values);
+
+				foreach (var dt in targets) {
+					if (dt.Valid) {
+						targetValues.Add ((dt.Label, dt.Value));
+					}
+				}
+
+				yield return wfs;
+			}
+		}
+
+
+	}
+
+	public struct DebugTarget {
+		private readonly Func<string> valueCallback;
+
+		public readonly string Label;
+
+		public bool Valid => !string.IsNullOrEmpty (Label) && valueCallback != null;
+		public string Value => valueCallback == null ? "" : valueCallback.Invoke ();
+
+		public DebugTarget (Func<string> valueCallback, string label) {
+			this.valueCallback = valueCallback;
+			Label = label;
+		}
+	}
+}
